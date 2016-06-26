e6580ca7196e329d1b7ae4d972ce69c581f45552
Daniel Trone <royhobbstn@yahoo.com>
2016-06-26 19:39:50 +0000

    added defaults and timer

--- routes/routes.js
+++ routes/routes.js
@@ -8,15 +8,16 @@ var appRouter = function(app) {
 
     app.get("/screenshot", function(req, res) {
 
-        var website = req.query.website;
-        var filename = req.query.filename;
+        var website = req.query.website || 'google.com';
+        var filename = req.query.filename || 'file';
+        var timer = req.query.timer || 5000; //5 sec default
 
         res.writeHead(200, {
             'Content-Type': 'image/png',
             'Content-Disposition': 'attachment; filename=' + filename + '.png'
         });
 
-        var renderStream = webshot(website);
+        var renderStream = webshot(website, {renderDelay: timer});
 
         renderStream.on('data', function(data) {
             res.write(data.toString('binary'), 'binary');

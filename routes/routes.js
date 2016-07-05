75f2c4c8e1b695a008d55cc41fde55e85ea6555a
Daniel Trone <royhobbstn@yahoo.com>
2016-07-05 14:17:53 +0000

    added width height options and defaults

--- routes/routes.js
+++ routes/routes.js
@@ -11,13 +11,23 @@ var appRouter = function(app) {
         var website = req.query.website || 'google.com';
         var filename = req.query.filename || 'file';
         var timer = req.query.timer || 5000; //5 sec default
+        var width = req.query.width || 800;
+        var height = req.query.height || 600;
 
         res.writeHead(200, {
             'Content-Type': 'image/png',
             'Content-Disposition': 'attachment; filename=' + filename + '.png'
         });
 
-        var renderStream = webshot(website, {renderDelay: timer});
+        var options = {
+            screenSize: {
+                width: width,
+                height: height
+            },
+            renderDelay: timer
+        };
+
+        var renderStream = webshot(website, options);
 
         renderStream.on('data', function(data) {
             res.write(data.toString('binary'), 'binary');
